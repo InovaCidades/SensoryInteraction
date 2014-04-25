30498517ee2e37149a37efe54bf69b4b8d6b9102
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-04-25 16:59:30 -0300

    JavaScript bug fixed.

--- Experiments/Pong/Assets/GameSetup.js
+++ Experiments/Pong/Assets/GameSetup.js
@@ -14,5 +14,5 @@ function Update () {
 
 	//Move each wall to its edge location
 	topWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f);
-	topWall.center = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3(0f, screen.height, 0f)).y + 0.5f);
+	topWall.center = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3(0f, Screen.height, 0f)).y + 0.5f);
 }
