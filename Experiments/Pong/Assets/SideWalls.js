f2c2ba16de46ec8504582c14a75ada869b139a84
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-05-27 15:28:20 -0300

    Point sound now working properly.

--- Experiments/Pong/Assets/SideWalls.js
+++ Experiments/Pong/Assets/SideWalls.js
@@ -3,6 +3,7 @@
 function OnTriggerEnter2D (hitInfo : Collider2D) {
 	if(hitInfo.name == "Ball") {
 		var wallName = transform.name;
+		audio.Play();
 		GameManager.Score(wallName);
 		hitInfo.gameObject.SendMessage ("ResetBall");
 	}
