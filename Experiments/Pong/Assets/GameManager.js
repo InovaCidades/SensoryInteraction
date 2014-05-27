90d9e5d50c873d894d7b70491c75490838d0bc8c
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-05-27 13:23:25 -0300

    Added the point counters for the players.

--- Experiments/Pong/Assets/GameManager.js
+++ Experiments/Pong/Assets/GameManager.js
@@ -4,5 +4,12 @@ static var playerScore01 : int = 0;
 static var playerScore02 : int = 0;
 
 function Score (wallName : String) {
-
+	if(wallName == "rightWall") {
+		playerScore01 += 1;
+	}
+	else {
+		playerScore02 += 1;
+	}
+	Debug.Log("Player Score 1 is " + playerScore01);
+	Debug.Log("Player Score 2 is " + playerScore02);
 }
