5c979f2b430751ffaffca162b4e686adfb30152e
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-05-27 16:29:43 -0300

    Reset button is now exactly in the middle.

--- Experiments/Pong/Assets/GameManager.js
+++ Experiments/Pong/Assets/GameManager.js
@@ -18,10 +18,10 @@ static function Score(wallName : String) {
 
 function OnGUI () {
 	GUI.skin = theSkin;
-	GUI.Label (new Rect (Screen.width/2-150-12, 20, 100, 100), "" + playerScore01);
-	GUI.Label (new Rect (Screen.width/2+150-12, 20, 100, 100), "" + playerScore02);
+	GUI.Label (new Rect (Screen.width/2-150-18, 20, 100, 100), "" + playerScore01);
+	GUI.Label (new Rect (Screen.width/2+150-18, 20, 100, 100), "" + playerScore02);
 
-	if (GUI.Button (new Rect (Screen.width/2, 35, 121, 53), "RESET")) {
+	if (GUI.Button (new Rect (Screen.width/2-121/2, 35, 121, 53), "RESET")) {
 	
 	}
 }
