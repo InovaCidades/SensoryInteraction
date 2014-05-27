9229814dd25079c31005ad04dde87adf6d20314a
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-05-27 13:57:35 -0300

    New GUI object for better interface.

--- Experiments/Pong/Assets/GameManager.js
+++ Experiments/Pong/Assets/GameManager.js
@@ -3,6 +3,8 @@
 static var playerScore01 : int = 0;
 static var playerScore02 : int = 0;
 
+var theSkin : GUISkin;
+
 static function Score(wallName : String) {
 	if(wallName == "rightWall") {
 		playerScore01 += 1;
@@ -15,6 +17,7 @@ static function Score(wallName : String) {
 }
 
 function OnGUI () {
+	GUI.skin = theSkin;
 	GUI.Label (new Rect (Screen.width/2-150, 20, 100, 100), "" + playerScore01);
 	GUI.Label (new Rect (Screen.width/2+150, 20, 100, 100), "" + playerScore02);
 }
