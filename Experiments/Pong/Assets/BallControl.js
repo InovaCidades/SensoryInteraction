423b6e94123193b984709f42e22e9c4e0c20bdaf
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-05-27 14:20:42 -0300

    Added some time before the ball begins to bounce.

--- Experiments/Pong/Assets/BallControl.js
+++ Experiments/Pong/Assets/BallControl.js
@@ -1,18 +1,26 @@
 ﻿#pragma strict
 
+var ballSpeed : float = 100;
+
+
 function Start () {
-	var randomNumber = Random.Range(0, 2);
-	
-	if (randomNumber <= 0.5) {
-		rigidbody2D.AddForce (new Vector2 (80, 10));
-	}
-	else {
-		rigidbody2D.AddForce (new Vector2 (-80, -10));
-	}
+	yield WaitForSeconds(2);
+	GoBall();
 }
 
 function OnCollisionEnter2D(colInfo : Collision2D) {
 	if (colInfo.collider.tag == "Player") {
 		rigidbody2D.velocity.y = rigidbody2D.velocity.y/2 + colInfo.collider.rigidbody2D.velocity.y/3;
+	}
+}
+
+function GoBall() {
+	var randomNumber = Random.Range(0, 2);
+	
+	if (randomNumber <= 0.5) {
+		rigidbody2D.AddForce (new Vector2 (ballSpeed, 10));
+	}
+	else {
+		rigidbody2D.AddForce (new Vector2 (-ballSpeed, -10));
 	}
 }
