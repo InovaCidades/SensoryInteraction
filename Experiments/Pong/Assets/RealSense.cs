ca3a10324cda858c687a8e1201424b1018d89c4d
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-06-05 16:36:40 -0300

    Started the RealSense™ integration.

---
+++ Experiments/Pong/Assets/RealSense.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RealSense : MonoBehaviour {
+
+	private PXCUPipeline pipe;
+	private PXCUPipeline.Mode   mode=PXCUPipeline.Mode.GESTURE;
+	
+	void Start () 
+	{
+		pipe = new PXCUPipeline(); 
+		if (!pipe.Init(mode)) 
+		{
+			print("Unable to initialize gesture mode");
+			return;
+		}
+	}
+	void Update()
+	{
+		if (!pipe.AcquireFrame(false)) 
+			return;
+		
+		pipe.ReleaseFrame();      
+	}
+
+	void OnDisable() 
+	{
+		pipe.Close();
+		pipe.Dispose ();
+	}
+}
